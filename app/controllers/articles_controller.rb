8874e76ea239d13317afa5733f7c40a5bb1ffc6e
DC Rails Course <dougjc@gmail.com>
2016-04-12 04:13:30 +0000

    Added bootstrap

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,18 +1,48 @@
 class ArticlesController < ApplicationController
     
+    before_action :set_article, only: [:edit, :update, :show, :destroy]
+    
     def new
         @article = Article.new
     end
     
+    def edit 
+    end
+    
     def create
-        #render plain: params[:article].inspect
         @article = Article.new(article_params)
-        @article.save
+        if @article.save
+            flash[:notice] = "Article successfully saved"
+            redirect_to article_path(@article)
+        else
+            render 'new'
+        end
+    end
+    
+    def update
+        if @article.update(article_params)
+            flash[:notice] = "Article was updated"
+            redirect_to article_path(@article)
+        else
+            render 'edit'
+        end
+    end
+    
+    def show
+    end
+    
+    def destroy
+        @article.destroy
+        flash[:notice] = "Article successfully deleted"
+        redirect_to_articles_path
     end
     
     private
-    def article_params
-        params.require(:article).permit(:title, :description)
-    end
+        def set_article
+            @article = Article.find(params[:id])
+        end
+        def article_params
+            params.require(:article).permit(:title, :description)
+        end
         
 end
